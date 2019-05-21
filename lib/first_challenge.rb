51f172337eaf721f810208fb084f6cb699564a1e
Reina Ewing <reina.castro3@gmail.com>
2019-05-21 18:53:32 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,6 +14,16 @@ def first_challenge
   }
 
   #your code here
+    contacts.each do |person, data|
+      data.each do |attributes, value|
+        if attributes == :favorite_icecream_flavors
+          values.delete_if do |flavor|
+             flavor.any?("strawberry")
+            end
+          end 
+        end 
+      end 
+             
     
 
   #remember to return your newly altered contacts hash!
